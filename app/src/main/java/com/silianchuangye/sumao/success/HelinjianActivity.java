a17112487e542c978a22c08b319076a5cc0c0869
15210183057 <15210183057@163.com>
2016-04-13 14:52:41 +0800

    hljActivity
    贺琳健的Activity

---
+++ app/src/main/java/com/silianchuangye/sumao/success/HelinjianActivity.java
@@ -0,0 +1,13 @@
+package com.silianchuangye.sumao.success;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelinjianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helinjian);
+    }
+}
