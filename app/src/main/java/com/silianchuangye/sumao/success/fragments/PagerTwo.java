d7bfe08bcec4a9e9aee5c733691fe038194afaa7
WangXinLong-Long <18401564300@126.com>
2016-04-21 16:03:34 +0800

    修改布局文件

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/PagerTwo.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/PagerTwo.java
@@ -2,6 +2,7 @@ package com.silianchuangye.sumao.success.fragments;
 
 import android.graphics.Color;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.silianchuangye.sumao.success.R;
 
@@ -9,9 +10,14 @@ import com.silianchuangye.sumao.success.R;
  * Created by Administrator on 2016/4/20 0020.
  */
 public class PagerTwo extends BasePager {
+    LinearLayout linearLayout;
     @Override
     public void initDate() {
-        fl_content.setBackgroundColor(getResources().getColor(R.color.red));
+//        fl_content.setBackgroundColor(getResources().getColor(R.color.red));
+        linearLayout = (LinearLayout) View.inflate(mActivity,R.layout.test_fragment,null);
+        fl_content.addView(linearLayout);
+
+
     }
 
     @Override
