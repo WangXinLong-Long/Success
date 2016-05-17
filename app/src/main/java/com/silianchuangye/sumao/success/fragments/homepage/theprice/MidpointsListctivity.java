bf3ef2935f0d9e27f83e7a8fd5de6d0559707d19
WangXinLong-Long <18401564300@126.com>
2016-05-17 11:06:03 +0800

    2016/5/17 首页，创建点价，预售，文件夹

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/homepage/theprice/MidpointsListctivity.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/homepage/theprice/MidpointsListctivity.java
@@ -1,4 +1,4 @@
-package com.silianchuangye.sumao.success.fragments.OrderManagement.SpotOrder;
+package com.silianchuangye.sumao.success.fragments.homepage.theprice;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
