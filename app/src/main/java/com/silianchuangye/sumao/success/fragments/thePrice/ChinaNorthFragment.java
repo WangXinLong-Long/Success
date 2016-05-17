665a7524f27a4f12f1652fb71259a3862f5fd8bc
15210183057 <15210183057@163.com>
2016-05-17 11:10:05 +0800

    贺琳健+2016/5/17+am 刚写一点点价界面，

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/thePrice/ChinaNorthFragment.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/thePrice/ChinaNorthFragment.java
@@ -6,15 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.silianchuangye.sumao.success.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+//华北界面
 public class ChinaNorthFragment extends Fragment {
-
-
+    private View chinanorth;
+    private TextView tv_chinanorth;
+    private ImageView img_chiannorth;
+    private ListView lv_chinanorth;
     public ChinaNorthFragment() {
         // Required empty public constructor
     }
@@ -24,7 +30,16 @@ public class ChinaNorthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_china_north, container, false);
+        chinanorth =inflater.inflate(R.layout.fragment_china_north, container, false);
+        initView();
+        return chinanorth;
+    }
+
+    private void initView() {
+        tv_chinanorth= (TextView) chinanorth.findViewById(R.id.tv_chinanorth);
+        img_chiannorth= (ImageView) chinanorth.findViewById(R.id.img_chinanorth);
+        lv_chinanorth= (ListView) chinanorth.findViewById(R.id.lv_chainanorth);
+
     }
 
 }
