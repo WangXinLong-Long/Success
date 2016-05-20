6998305692cc43a309288d586ea2d5bba7aa5e37
guanjun123 <2414784489@qq.com>
2016-05-20 09:45:59 +0800

    补充刚才的揭界面

---
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/auction/ContractFragment.java
@@ -0,0 +1,30 @@
+package com.silianchuangye.sumao.success.fragments.auction;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.silianchuangye.sumao.success.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContractFragment extends Fragment {
+
+
+    public ContractFragment() {
+        // Required empty public constructorll
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contract, container, false);
+    }
+
+}
