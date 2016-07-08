dea1f6bbdbab3567a7adc5e3377640aa87c83826
15210183057 <15210183057@163.com>
2016-07-08 11:20:47 +0800

    贺琳健+2016/7/7+pm 物流界面popwindow弹框功能

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/logistics/CreateLogistics.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/logistics/CreateLogistics.java
@@ -4,7 +4,6 @@ import android.app.DatePickerDialog;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
