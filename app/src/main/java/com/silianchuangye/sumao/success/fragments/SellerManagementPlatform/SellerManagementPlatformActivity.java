f0b9eba7fd65780f36cf1c67e45112830d874146
WangXinLong-Long <18401564300@126.com>
2016-07-13 11:24:25 +0800

    2016/7/13 我是卖家的平台

---
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/SellerManagementPlatform/SellerManagementPlatformActivity.java
@@ -0,0 +1,18 @@
+package com.silianchuangye.sumao.success.fragments.SellerManagementPlatform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.silianchuangye.sumao.success.R;
+
+/**
+ * Created by Administrator on 2016/7/13 0013.
+ */
+public class SellerManagementPlatformActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seller_management_platform_activity);
+    }
+}
