19e7b051f911563dd139d8a926cf129f903db9c0
15210183057 <15210183057@163.com>
2016-08-08 16:09:50 +0800

    2016/8/8+pm 贺琳健订单接口预售和客服

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/OrderManagement/SpotOrder/OrderNetutils.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/OrderManagement/SpotOrder/OrderNetutils.java
@@ -1,22 +1,14 @@
 package com.silianchuangye.sumao.success.fragments.myPlasticTrade.OrderManagement.SpotOrder;
 
-
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.os.Message;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ExpandableListView;
 import android.widget.Toast;
-
-import com.silianchuangye.sumao.success.R;
+import android.os.Handler;
 import com.silianchuangye.sumao.success.adapter.MyAdapter;
-import com.silianchuangye.sumao.success.fragments.myPlasticTrade.OrderManagement.OrderDetails.AlreadyPaidActivity;
-import com.silianchuangye.sumao.success.utils.LogUtils;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,119 +23,28 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Created by Administrator on 2016/8/8.
  */
-public class PresellallFragment extends Fragment {
-    private ExpandableListView elvDemo;
-//    private List<Map<String,Object>> listparrent;
-//    private List<List<Map<String,Object>>> listitem;
-    private String state1;
+public class OrderNetutils {
     private List<Map<String,Object>> listparrent=new ArrayList<Map<String,Object>>();
     private List<List<Map<String,Object>>> listitem=new ArrayList<List<Map<String,Object>>>();
-    MyAdapter adapter;
-    private String url="http://192.168.32.126:7023/rest/model/atg/userprofiling/ProfileActor/myOrders";
-   private OrderNetutils orderNetutils;
-    public PresellallFragment() {
-        // Required empty public constructor
+    String state1;
+    private Context ctx;
+    String url;//网络地址
+    String searchOrderType,searchOrderState;//订单类型---//订单状态
+    int pageNum,submitType;//页码，查询类型
+    private List list=new ArrayList();
+    public OrderNetutils(Context ctx){
+        this.ctx=ctx;
     }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_presellall, container, false);
-        //实例化
-        elvDemo= (ExpandableListView) view.findViewById(R.id.elvDemo);
-        //去掉expandListview的特别的下拉标志
-        elvDemo.setGroupIndicator(null);
-        //去掉ListView之间的线
-        elvDemo.setDivider(null);
-        orderNetutils=new OrderNetutils(getActivity());
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                sendMy();
-            }
-        }.start();
-//        listparrent=new ArrayList<Map<String,Object>>();
-//        Map<String,Object> map1=new Hashtable<String,Object>();
-//        map1.put("id","1000001");
-//        map1.put("price","70000.0");
-//        map1.put("states","待支付");
-//        map1.put("name","张三");
-//        listparrent.add(map1);
-//        Map<String,Object> map2=new Hashtable<String,Object>();
-//        map2.put("id","1000001");
-//        map2.put("price","88888888");
-//        map2.put("states","已支付");
-//        map2.put("name","李四");
-//        listparrent.add(map2);
-//
-//        listitem=new ArrayList<List<Map<String,Object>>>();
-//        final List<Map<String,Object>> list1=new ArrayList<Map<String,Object>>();
-//        Map<String,Object> map=new Hashtable<String,Object>();
-//        map.put("type","四联创业");
-//        map.put("name","中国");
-//        Map<String,Object> map3=new Hashtable<String,Object>();
-//        map3.put("type","四联创业");
-//        map3.put("name","中国");
-//        Map<String,Object> map4=new Hashtable<String,Object>();
-//        map4.put("type","四联创业");
-//        map4.put("name","中国");
-//        list1.add(map);
-//        list1.add(map3);
-//        list1.add(map4);
-//
-//        List<Map<String,Object>> list2=new ArrayList<Map<String,Object>>();
-//
-//        Map<String,Object> map5=new Hashtable<String,Object>();
-//        map5.put("type","四联创业");
-//        map5.put("name","中国");
-//
-//        list2.add(map5);
-//
-//        listitem.add(list1);
-//        listitem.add(list2);
-//
-//
-//        MyAdapter adapter=new MyAdapter(listparrent,listitem,getActivity());
-//        elvDemo.setAdapter(adapter);
-//        if(adapter!=null && listparrent!=null){
-//            for (int i = 0; i < listparrent.size(); i++) {
-//                elvDemo.expandGroup(i);
-//            }}
-        elvDemo.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-//                Toast.makeText(getContext(), "点击title", Toast.LENGTH_SHORT).show();
-                LogUtils.log(listparrent.get(groupPosition).get("states")+"");
-                if ("已支付".equals(listparrent.get(groupPosition).get("states"))){
-                    Intent intent = new Intent();
-                    intent.setClass(getActivity(),AlreadyPaidActivity.class);
-                    startActivity(intent);
-                }else if("待支付".equals(listparrent.get(groupPosition).get("states"))){
-                    Intent intent = new Intent();
-                    intent.setClass(getActivity(), SpotOrder.class);
-                    startActivity(intent);
-                }
-
-                return true;
-
-            }
-        });
-
-        return view;
-    }
-    private void sendMy(){
+    public List sendMy(String url,int pageNum,int submitType,String searchOrderType,String searchOrderState){
         RequestParams params=new RequestParams(url);
-        params.addParameter("pageNum",1);
-        params.addParameter("submitType",1);
-        params.addParameter("searchOrderType","forwardPricingOrder");
-        params.addParameter("searchOrderState",0);
+        params.addParameter("pageNum",pageNum);
+        params.addParameter("submitType",submitType);
+        params.addParameter("searchOrderType",searchOrderType);
+        params.addParameter("searchOrderState",searchOrderState);
 //        params.addParameter("searchCompanyName",company);
-        final SharedPreferences sp = getActivity().getSharedPreferences("sumao", Activity.MODE_PRIVATE);
+        final SharedPreferences sp = ctx.getSharedPreferences("sumao", Activity.MODE_PRIVATE);
         String unique123 = sp.getString("unique", "");
         params.addParameter("_dynSessConf", unique123);
         Log.e("TAG","parames======"+params);
@@ -155,7 +56,7 @@ public class PresellallFragment extends Fragment {
                     JSONObject job = new JSONObject(result);
                     String info = job.getString("info");
                     if (info.equals("fail")) {
-                        Toast.makeText(getActivity(), "获取数据失败", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ctx, "获取数据失败", Toast.LENGTH_SHORT).show();
                     } else {
                         String str = job.getString("order");
                         JSONArray jay = new JSONArray(str);
@@ -215,19 +116,15 @@ public class PresellallFragment extends Fragment {
                             map1.put("name", owner);
                             Log.e("TAG", "map1-----" + map1);
                             listparrent.add(map1);
+                            list.add(listitem);
+                            list.add(listparrent);
+                            Log.e("TAG","list==="+list.size());
                         }
                     }
-                    adapter = new MyAdapter(listparrent, listitem, getActivity());
-                    elvDemo.setAdapter(adapter);
-                    if (adapter != null && listparrent != null) {
-                        for (int i = 0; i < listparrent.size(); i++) {
-                            elvDemo.expandGroup(i);
-                        }
-                    }
-
                 }catch(JSONException e){
                     e.printStackTrace();
                 }
+
             }
             @Override
             public void onError(Throwable ex, boolean isOnCallback) {
@@ -245,5 +142,6 @@ public class PresellallFragment extends Fragment {
 
             }
         });
+        return  list;
     }
 }
--- app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/OrderManagement/SpotOrder/OrderallFragment.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/OrderManagement/SpotOrder/OrderallFragment.java
@@ -36,10 +36,10 @@ import java.util.Map;
  */
 public class OrderallFragment extends Fragment {
     private ExpandableListView elvDemo;
-    private List<Map<String,Object>> listparrent=new ArrayList<Map<String,Object>>();;
-    private List<List<Map<String,Object>>> listitem=new ArrayList<List<Map<String,Object>>>();;
-//    private List<Map<String,Object>> alllistparrent=new ArrayList<Map<String,Object>>();
-//    private List<List<Map<String,Object>>> alllistitem=new ArrayList<List<Map<String,Object>>>() ;
+//    private List<Map<String,Object>> listparrent=new ArrayList<Map<String,Object>>();;
+//    private List<List<Map<String,Object>>> listitem=new ArrayList<List<Map<String,Object>>>();;
+    private List<Map<String,Object>> listparrent;
+    private List<List<Map<String,Object>>> listitem;
     MyAdapter adapter;
     String state1;
     public OrderallFragment() {
@@ -62,55 +62,59 @@ public class OrderallFragment extends Fragment {
         new Thread(){
             @Override
             public void run() {
-                Log.e("TAG","therw");
                 super.run();
                 sendMy();
             }
         }.start();
 
-//        listparrent=new ArrayList<Map<String,Object>>();
-//        Map<String,Object> map1=new Hashtable<String,Object>();
-//        map1.put("id","1000001");
-//        map1.put("price","70000.0");
-//        map1.put("states","待支付1");
-//        map1.put("p","123213");
-//        listparrent.add(map1);
-//        Map<String,Object> map2=new Hashtable<String,Object>();
-//        map2.put("id","1000001");
-//        map2.put("price","88888888");
-//        map2.put("states","已支付2");
-//        map2.put("p","123");
-//        listparrent.add(map2);
-//
-//        listitem=new ArrayList<List<Map<String,Object>>>();
-//        List<Map<String,Object>> list1=new ArrayList<Map<String,Object>>();
-//        Map<String,Object> map=new Hashtable<String,Object>();
-//        map.put("type","四联创业");
-//
-//        map.put("name","中国");
-//        Map<String,Object> map3=new Hashtable<String,Object>();
-//        map3.put("type","四联创业");
-//        map3.put("name","中国");
-//        Map<String,Object> map4=new Hashtable<String,Object>();
-//        map4.put("type","四联创业");
-//        map4.put("name","中国");
-//        list1.add(map);
-//        list1.add(map3);
-//        list1.add(map4);
-//
-//        List<Map<String,Object>> list2=new ArrayList<Map<String,Object>>();
-//
-//        Map<String,Object> map5=new Hashtable<String,Object>();
-//        map5.put("type","四联创业");
-//        map5.put("name","中国");
-//
-//        list2.add(map5);
-//
-//        listitem.add(list1);
-//        listitem.add(list2);
-//
-//        adapter=new MyAdapter(listparrent,listitem,getActivity());
-//        elvDemo.setAdapter(adapter);
+        listparrent=new ArrayList<Map<String,Object>>();
+        Map<String,Object> map1=new Hashtable<String,Object>();
+        map1.put("id","1000001");
+        map1.put("price","70000.0");
+        map1.put("states","待支付1");
+        map1.put("name","张三");
+        listparrent.add(map1);
+        Map<String,Object> map2=new Hashtable<String,Object>();
+        map2.put("id","1000001");
+        map2.put("price","88888888");
+        map2.put("states","已支付2");
+        map2.put("name","张帝");
+        listparrent.add(map2);
+
+        listitem=new ArrayList<List<Map<String,Object>>>();
+        List<Map<String,Object>> list1=new ArrayList<Map<String,Object>>();
+        Map<String,Object> map=new Hashtable<String,Object>();
+        map.put("type","四联创业");
+
+        map.put("name","中国");
+        Map<String,Object> map3=new Hashtable<String,Object>();
+        map3.put("type","四联创业");
+        map3.put("name","中国");
+        Map<String,Object> map4=new Hashtable<String,Object>();
+        map4.put("type","四联创业");
+        map4.put("name","中国");
+        list1.add(map);
+        list1.add(map3);
+        list1.add(map4);
+
+        List<Map<String,Object>> list2=new ArrayList<Map<String,Object>>();
+
+        Map<String,Object> map5=new Hashtable<String,Object>();
+        map5.put("type","四联创业");
+        map5.put("name","中国");
+
+        list2.add(map5);
+
+        listitem.add(list1);
+        listitem.add(list2);
+
+        adapter=new MyAdapter(listparrent,listitem,getActivity());
+        elvDemo.setAdapter(adapter);
+        if (adapter != null && listparrent != null) {
+            for (int i = 0; i < listparrent.size(); i++) {
+                elvDemo.expandGroup(i);
+            }
+        }
         elvDemo.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
             @Override
             public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
@@ -147,11 +151,11 @@ public class OrderallFragment extends Fragment {
             public void onSuccess(String result) {
                 Log.e("TAG","result----"+result);
                 try {
-                    JSONObject job=new JSONObject(result);
-                    String info=job.getString("info");
-                    if(info.equals("fail")){
-                        Toast.makeText(getActivity(),"获取数据失败",Toast.LENGTH_SHORT).show();
-                    }else {
+                    JSONObject job = new JSONObject(result);
+                    String info = job.getString("info");
+                    if (info.equals("fail")) {
+                        Toast.makeText(getActivity(), "获取数据失败", Toast.LENGTH_SHORT).show();
+                    } else {
                         String str = job.getString("order");
                         JSONArray jay = new JSONArray(str);
                         for (int i = 0; i < jay.length(); i++) {
@@ -189,40 +193,41 @@ public class OrderallFragment extends Fragment {
                             String owner = j.getString("owner");//采购员
                             String orderId = j.getString("orderId");//订单编号
 
-                        JSONArray j1=new JSONArray(cl);
-                        for(int k=0;k<j1.length();k++){
-                            JSONObject job1= (JSONObject) j1.get(k);
-                            cl_amount=job1.getString("cl_amount");//金额
-                            String cl_mingcheng=job1.getString("cl_mingcheng");//产品名称
-                            String cl_fenlei=job1.getString("cl_fenlei");
-                            Log.e("TAG","mingc=="+cl_mingcheng);
-                            List<Map<String,Object>> list1=new ArrayList<Map<String,Object>>();
-                            Map<String,Object> map=new Hashtable<String,Object>();
-                            map.put("type",cl_fenlei);
-                            map.put("name",cl_mingcheng);
-                            list1.add(map);
-                            listitem.add(list1);
+                            JSONArray j1 = new JSONArray(cl);
+                            for (int k = 0; k < j1.length(); k++) {
+                                JSONObject job1 = (JSONObject) j1.get(k);
+                                cl_amount = job1.getString("cl_amount");//金额
+                                String cl_mingcheng = job1.getString("cl_mingcheng");//产品名称
+                                String cl_fenlei = job1.getString("cl_fenlei");
+                                Log.e("TAG", "mingc==" + cl_mingcheng);
+                                List<Map<String, Object>> list1 = new ArrayList<Map<String, Object>>();
+                                Map<String, Object> map = new Hashtable<String, Object>();
+                                map.put("type", cl_fenlei);
+                                map.put("name", cl_mingcheng);
+                                list1.add(map);
+                                listitem.add(list1);
+                            }
+                            Map<String, Object> map1 = new Hashtable<String, Object>();
+                            map1.put("id", orderId);
+                            map1.put("price", cl_amount);
+                            map1.put("states", state1);
+                            map1.put("name", owner);
+                            Log.e("TAG", "map1-----" + map1);
+                            listparrent.add(map1);
                         }
-                        Map<String,Object> map1=new Hashtable<String,Object>();
-                        map1.put("id",orderId);
-                        map1.put("price",cl_amount);
-                        map1.put("states",state1);
-                        map1.put("p",owner);
-                        Log.e("TAG","map1-----"+map1);
-                        listparrent.add(map1);
                     }
-                    adapter=new MyAdapter(listparrent,listitem,getActivity());
-                    elvDemo.setAdapter(adapter);
-                    if(adapter!=null && listparrent!=null){
-                        for (int i = 0; i < listparrent.size(); i++) {
-                            elvDemo.expandGroup(i);
-                        }}
+                        adapter = new MyAdapter(listparrent, listitem, getActivity());
+                        elvDemo.setAdapter(adapter);
+                        if (adapter != null && listparrent != null) {
+                            for (int i = 0; i < listparrent.size(); i++) {
+                                elvDemo.expandGroup(i);
+                            }
+                        }
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    }catch(JSONException e){
+                        e.printStackTrace();
+                    }
                 }
-            }
-
             @Override
             public void onError(Throwable ex, boolean isOnCallback) {
                 Log.e("TAG","ex==="+ex);
