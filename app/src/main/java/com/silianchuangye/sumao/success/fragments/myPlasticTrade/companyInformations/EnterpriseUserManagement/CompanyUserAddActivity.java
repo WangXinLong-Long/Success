a8028eb47844e9eb1dabb9384523adc70087dcf3
guanjun123 <2414784489@qq.com>
2016-08-17 11:13:58 +0800

    企业用户管理的添加，修改接口，解决注册时跳转崩溃

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/companyInformations/EnterpriseUserManagement/CompanyUserAddActivity.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/companyInformations/EnterpriseUserManagement/CompanyUserAddActivity.java
@@ -1,10 +1,13 @@
 package com.silianchuangye.sumao.success.fragments.myPlasticTrade.companyInformations.EnterpriseUserManagement;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -15,9 +18,15 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.silianchuangye.sumao.success.R;
+import com.silianchuangye.sumao.success.fragments.myPlasticTrade.companyInformations.firmInfomation.FirmInfoTypeActivity;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -34,6 +43,9 @@ public class CompanyUserAddActivity extends AppCompatActivity {
     Button sv_title_bar_serachView;
     TextView tv_title_bar_title,tv;
     RelativeLayout layoutTop;
+    private SimpleAdapter adapter;
+    private String account,phone,email,name,role,state;
+    private Button btsave_company_user_add;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -46,39 +58,45 @@ public class CompanyUserAddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_user_add);
         title_Bar();
+
         lVadd = (ListView) findViewById(R.id.lvAdd_company_user);
         list = new ArrayList<Map<String, Object>>();
-        Map<String, Object> map = new Hashtable<String, Object>();
-        map.put("text", "登录账号");
-        // map.put("is","");
+        Map<String,Object> map=new Hashtable<String,Object>();
+        map.put("text","登录账号");
+        map.put("right","");
         list.add(map);
-        Map<String, Object> map1 = new Hashtable<String, Object>();
-        map1.put("text", "密码");
-        // map.put("is","");
+
+        Map<String,Object> map1=new Hashtable<String,Object>();
+        map1.put("text","姓名");
+        map1.put("right","");
         list.add(map1);
-        Map<String, Object> map2 = new Hashtable<String, Object>();
-        map2.put("text", "姓名");
-        // map.put("is","");
+
+        Map<String,Object> map2=new Hashtable<String,Object>();
+        map2.put("text","电话号码");
+        map2.put("right","");
         list.add(map2);
-        Map<String, Object> map3 = new Hashtable<String, Object>();
-        map3.put("text", "手机号码");
-        // map.put("is","");
+
+        Map<String,Object> map3=new Hashtable<String,Object>();
+        map3.put("text","邮箱");
+        map3.put("right","");
         list.add(map3);
-        Map<String, Object> map4 = new Hashtable<String, Object>();
-        map4.put("text", "邮箱");
+
+        Map<String,Object> map4=new Hashtable<String,Object>();
+        map4.put("text","职责");
+        map4.put("right","");
+        map4.put("icon",R.mipmap.my_sumao_iv_order);
         list.add(map4);
-        Map<String, Object> map5 = new Hashtable<String, Object>();
-        map5.put("text", "职责");
-
-        map5.put("icon", R.mipmap.my_sumao_iv_order);
-        list.add(map5);
-        Map<String, Object> map6 = new Hashtable<String, Object>();
-        map6.put("text", "状态");
-        map6.put("icon", R.mipmap.my_sumao_iv_order);
-        list.add(map6);
 
 
-        Adapter adapter = new Adapter(CompanyUserAddActivity.this, list, R.layout.item_company_user_add, new String[]{"text", "icon"}, new int[]{R.id.tvText_company_user_add, R.id.ivMore_company_user_add});
+
+
+        adapter = new SimpleAdapter(CompanyUserAddActivity.this, list, R.layout.item_company_user_add,
+                          new String[]{"text","right", "icon"},
+                          new int[]{
+                                  R.id.tvText_company_user_add,
+                                  R.id.etContext_company_user,
+                                  R.id.ivMore_company_user_add
+                          });
         lVadd.setAdapter(adapter);
 
 
@@ -86,16 +104,123 @@ public class CompanyUserAddActivity extends AppCompatActivity {
            @Override
            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-               if (position==5){
+               if (position==4){
                    Intent intent=new Intent(CompanyUserAddActivity.this,CompanyUserDutyActivity.class);
-                   startActivity(intent);
-               }else if (position==6){
-                   Intent intent=new Intent(CompanyUserAddActivity.this,CompanyUserStateActivity.class);
-                   startActivity(intent);
+                   startActivityForResult(intent,4);
+               }else if (position==0||position==1||position==2||position==3){
+                   Intent intent=new Intent(CompanyUserAddActivity.this,CompanyUserValueActivity.class);
+                   intent.putExtra("title",list.get(position).get("text").toString());
+                   intent.putExtra("number",position);
+                   startActivityForResult(intent,position);
                }
+
            }
        });
+        btsave_company_user_add= (Button) findViewById(R.id.btsave_company_user_add);
+        btsave_company_user_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                new Thread(){
+//                    @Override
+//                    public void run() {
+//                        addUser();
+//                    }
+//                }.start();
+                String url=
+                        "http://192.168.32.126:7023/rest/model/atg/userprofiling/ProfileActor/addUser";
+                RequestParams rp=new RequestParams(url);
+                rp.addParameter("cl_cpid",account);
+                rp.addParameter("cl_mingcheng",name);
+                rp.addParameter("cl_shuliang",email);
+                rp.addParameter("cl_jiner",phone);
+                String role_id="";
+                if (role.contains("企业管理员")){
+                    role_id=role_id+" 50";
+                }
+                if (role.contains("财务员")){
+                    role_id=role_id+" 53";
+                }
+                if (role.contains("业务员")){
+                    role_id=role_id+" 52";
+                }
+                rp.addParameter("cl_qiye","50");
+                SharedPreferences sp=getSharedPreferences("sumao", Activity.MODE_PRIVATE);
+                String unique=sp.getString("unique","");
+                Log.d("unique",unique);
+                rp.addParameter("_dynSessConf",unique);
+                Log.d("rp",rp.toString());
+
+                x.http().post(rp, new Callback.CommonCallback<String>() {
+                    @Override
+                    public void onSuccess(String result) {
+                        Log.d("reult",result);
+                        if (result.contains("sucess")){
+                            Toast.makeText(CompanyUserAddActivity.this, "您已注册成功", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }else{
+                            Toast.makeText(CompanyUserAddActivity.this, "请重新输入您的信息", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(Throwable ex, boolean isOnCallback) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(CancelledException cex) {
+
+                    }
+
+                    @Override
+                    public void onFinished() {
+
+                    }
+                });
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 0:
+                account=data.getStringExtra("content");
+                Log.d("account",account);
+                list.get(0).put("right",account);
+                adapter.notifyDataSetChanged();
+                break;
+            case 1:
+                name=data.getStringExtra("content");
+                Log.d("account",name);
+                list.get(1).put("right",name);
+                adapter.notifyDataSetChanged();
+                break;
+            case 2:
+                phone=data.getStringExtra("content");
+                Log.d("account",phone);
+                list.get(2).put("right",phone);
+                adapter.notifyDataSetChanged();
+                break;
+            case 3:
+                email=data.getStringExtra("content");
+                Log.d("account",email);
+                list.get(3).put("right",email);
+                adapter.notifyDataSetChanged();
+                break;
+            case 4:
+                role=data.getStringExtra("people");
+                Log.d("account",role);
+                list.get(4).put("right",role);
+                adapter.notifyDataSetChanged();
+                break;
+
+        }
+    }
+
     public void title_Bar(){
         iv_title_bar_back = ((ImageView) findViewById(R.id.iv_title_bar_back));
         iv_title_bar_logo = ((ImageView) findViewById(R.id.iv_title_bar_logo));
@@ -122,53 +247,57 @@ public class CompanyUserAddActivity extends AppCompatActivity {
         });
     }
 
+    public void addUser(){
 
-    class Adapter extends SimpleAdapter {
-
-
-        public Adapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
-            super(context, data, resource, from, to);
-        }
-
-        @Override
-        public int getCount() {
-            return list.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return list.get(position);
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            convertView = super.getView(position, convertView, parent);
-
-            if (convertView != null) {
-                convertView = getLayoutInflater().inflate(R.layout.item_company_user_add, null);
-                EditText et = (EditText) convertView.findViewById(R.id.etContext_company_user);
-                RelativeLayout layout =(RelativeLayout)convertView.findViewById(R.id.layouttop_add_item);
-                TextView tv= (TextView) convertView.findViewById(R.id.tvText_company_user_add);
-                EditText et_pass= (EditText) convertView.findViewById(R.id.etContext_company_user_pass);
-                tv.setText(list.get(position).get("text").toString());
-                ImageView iv= (ImageView) convertView.findViewById(R.id.ivMore_company_user_add);
-                if (position==5||position==6){
-                    iv.setVisibility(View.VISIBLE);
-                    et.setVisibility(View.INVISIBLE);
-                }else{
-                    iv.setVisibility(View.GONE);
-                }
-                if(position==1){
-                    et_pass.setVisibility(View.VISIBLE);
-                    et.setVisibility(View.GONE);
-
-                }
-            }
-
-
-
-
-            return convertView;
-        }
     }
+
+
+//    class Adapter extends SimpleAdapter {
+//
+//
+//        public Adapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+//            super(context, data, resource, from, to);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return list.size();
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return list.get(position);
+//        }
+//
+//        @Override
+//        public View getView(final int position, View convertView, ViewGroup parent) {
+//            convertView = super.getView(position, convertView, parent);
+//
+//            if (convertView != null) {
+//                convertView = getLayoutInflater().inflate(R.layout.item_company_user_add, null);
+//                TextView et = (TextView) convertView.findViewById(R.id.etContext_company_user);
+//                RelativeLayout layout =(RelativeLayout)convertView.findViewById(R.id.layouttop_add_item);
+//                TextView tv= (TextView) convertView.findViewById(R.id.tvText_company_user_add);
+//              //  EditText et_pass= (EditText) convertView.findViewById(R.id.etContext_company_user_pass);
+//                tv.setText(list.get(position).get("text").toString());
+//                ImageView iv= (ImageView) convertView.findViewById(R.id.ivMore_company_user_add);
+//                if (position==5||position==6){
+//                    iv.setVisibility(View.VISIBLE);
+//                    et.setVisibility(View.INVISIBLE);
+//                }else{
+//                    iv.setVisibility(View.GONE);
+//                }
+//                if(position==1){
+//                    //et_pass.setVisibility(View.VISIBLE);
+//                    et.setVisibility(View.GONE);
+//
+//                }
+//            }
+//
+//
+//
+//
+//            return convertView;
+//        }
+//    }
 }
