14a6e0a8ca260c06dbc8de9c7339dc0da3b581fc
guanjun123 <2414784489@qq.com>
2016-08-17 11:45:48 +0800

    提交

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/homepage/goodInStock/GoodsInStockActivityMVP/model/IGoodsInStockActivityModel.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/homepage/goodInStock/GoodsInStockActivityMVP/model/IGoodsInStockActivityModel.java
@@ -1,8 +1,6 @@
 package com.silianchuangye.sumao.success.fragments.homepage.goodInStock.GoodsInStockActivityMVP.model;
 
-/**
- * Created by junjun on 2016/8/15.
- */
+
 public interface IGoodsInStockActivityModel {
 
     void getGoodsInStockActivityResult(GoodsInStockActivityCallback callback);
