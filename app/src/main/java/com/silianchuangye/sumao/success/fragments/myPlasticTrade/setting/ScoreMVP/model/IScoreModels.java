5b0605e45b9545c628b9eb7605c61a349b2e5f4e
15210183057 <15210183057@163.com>
2016-10-19 10:00:17 +0800

    团购url

--- app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/setting/ScoreMVP/model/IScoreModels.java
+++ app/src/main/java/com/silianchuangye/sumao/success/fragments/myPlasticTrade/setting/ScoreMVP/model/IScoreModels.java
@@ -3,6 +3,6 @@ package com.silianchuangye.sumao.success.fragments.myPlasticTrade.setting.ScoreM
 /**
  * Created by Administrator on 2016/9/27 0027.
  */
-public interface IScoreModel {
+public interface IScoreModels {
     void sendScoreInServer(ScoreCallback callback);
 }
